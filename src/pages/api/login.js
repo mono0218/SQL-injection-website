4092ee05cdc26916b4c26b32cac06e13cc03c5be
mono0218 <81796635+mono0218@users.noreply.github.com>
2023-05-14 09:38:04 +0000

    version 1.0.0

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,32 +1,37 @@
-import { db } from './db';
-const mysql = require('mysql');
+const sql = require('mssql');
+
+const config = {
+    user: process.env.DB_USER, // better stored in an app setting such as process.env.DB_USER
+    password: process.env.DB_PASS, // better stored in an app setting such as process.env.DB_PASSWORD
+    server: process.env.DB_HOST, // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: process.env.DB_DATABASE, // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-
-        db.connect((err) => {
-            if (err) {
-              console.log('error connecting: ' + err.stack);
-              return;
-            }
-            console.log('success');
-          });
-
-        const result = await db.query(`SELECT * FROM users WHERE username = ${req.body.name} AND password = ${req.body.pass}`)
-
-        console.log(result._results[0])
-
-        if (result.error) {
-            res.status(500).json({ success: false });
-            return;
-        }
-    
-        // レスポンスの送信
-        if (result._results > 0) {
+      try {
+        const poolConnection = await sql.connect(config);
+        const result = await poolConnection.request().query(`SELECT * FROM users WHERE username = '${req.body.name}'  AND password ='${req.body.pass}';`)
+        if (result.rowsAffected > 0) {
             res.json({ success: true });
         } else {
-            res.json({ success: false });
+            res.json({ success: false ,error: false});
         }
+
+      } catch (error) {
+        if(error.code ==='EREQUEST'){
+          res.json({ success: false ,error: false});
+        }else{
+          res.json({ success: false ,error: true});
+        }
+      }
 
       } else {
         res.status(404).json({ message: 'Not found' });
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,12 @@ export default function LoginPage() {
 
   const onSubmit = async data => {
     const res = await axios.post('/api/login', data);
-    if(res.status===500){
-      alert('SQL接続時にエラーが発生しました');
+    if(res.data.success === true){
+      alert('成功しました')
+    }else if (res.data.success === false && res.data.error === false) {
+      alert('ログインに失敗しました')
+    } else {
+      alert('サーバーエラーが発生しました')
     }
   }
   return (
@@ -16,7 +20,7 @@ export default function LoginPage() {
       <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <input  {...register("name",{ required: true })} />
-      
+        
       {/* include validation with required or other standard HTML validation rules */}
       <input {...register("pass", { required: true })} />
       {/* errors will return when field validation fails  */}
